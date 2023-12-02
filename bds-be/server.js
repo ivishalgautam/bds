c7dfc470dd81ad2243cd2d7808894e9c5522618d
vishal <ivishalgautamm@gmail.com>
2023-12-02 17:55:08 +0530

    added buddy-team chat

--- bds-be/server.js
+++ bds-be/server.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from "url";
 import cors from "@fastify/cors";
 import { dirname } from "path";
 import path from "path";
+import fastifySocketIO from "@fastify/websocket";
 
 // import internal modules
 import authRoutes from "./app/api/auth/routes.js";
@@ -19,10 +20,42 @@ export default (app) => {
     root: path.join(dirname(fileURLToPath(import.meta.url)), "public"),
     prefix: "/public/",
   });
+  app.register(pg_database);
   app.register(fastifyMultipart);
   app.register(cors, { origin: "*" });
-  app.register(pg_database);
   app.register(routes, { prefix: "v1" });
   app.register(authRoutes, { prefix: "v1/auth" });
   app.register(uploadFileRoutes, { prefix: "v1/upload" });
+
+  app.register(fastifySocketIO, {
+    cors: { origin: "*" },
+    options: {
+      maxPayload: 1048576,
+      clientTracking: true,
+    },
+  });
+
+  app.register(async function (app) {
+    app.get("/*", { websocket: true }, (connection, req) => {
+      connection.socket.on("message", (data) => {
+        const messageString = JSON.parse(data.toString("utf-8"));
+
+        app.websocketServer.clients.forEach((client) => {
+          client.send(
+            JSON.stringify({
+              group_chat_id: messageString.group_id,
+              message_from_id: messageString.user.id,
+              image_url: messageString.user.image_url,
+              message_from_fullname: `${messageString.user.first_name} ${messageString.user.last_name}`,
+              role: messageString.user.role,
+              profession: messageString.user.profession,
+              message: messageString.message,
+              created_at: new Date().toISOString(),
+              updated_at: new Date().toISOString(),
+            })
+          );
+        });
+      });
+    });
+  });
 };
